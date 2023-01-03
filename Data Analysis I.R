d7c37d903c0d6e903ced2cb3df716d0e6b60d194
smaach <meetsavsani2000@gmail.com>
2023-01-03 17:19:56 +0530

    second commit

--- Data Analysis I.R
+++ Data Analysis I.R
@@ -25,8 +25,26 @@ length(gapminder) # --> same as ncol()
 dim(gapminder) # --> get the dimensions of dataset
 nrow(gapminder) # --> get the no.of.rows
 
+
+#understanding the data values
+
+head(gapminder$country)
+summary(gapminder$lifeExp)
+head(gapminder$year)
+summary(gapminder$year)
+table(gapminder$year)
+class(gapminder$continent)
+levels(gapminder$continent)
+summary(gapminder$continent)
+
+
 #very basic plots in R
 
 ggplot(gapminder, aes(year, lifeExp)) + geom_point()
 ggplot(gapminder, aes(gdpPercap, lifeExp)) + geom_point()
 plot(lifeExp ~ gdpPercap, gapminder)
+plot(lifeExp ~ log(gdpPercap), gapminder)
+hist(gapminder$lifeExp)
+
+
+
