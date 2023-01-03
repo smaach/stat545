5903e9e0451070cce45ce2b90f969aea0e41b793
smaach <meetsavsani2000@gmail.com>
2023-01-03 17:56:26 +0530

    second commit

--- Data Analysis I.R
+++ Data Analysis I.R
@@ -2,14 +2,14 @@
 #while learning from the course stat545
 #url: https://stat545.com/
 
-#ch5: Data Analysis
+#ch5: Data Analysis I
 
 #install necessary packages
 
-install.packages("gapminder")
-library(gapminder)
-install.packages("tidyverse")
-library(tidyverse)
+# install.packages("gapminder")
+# library(gapminder)
+# install.packages("tidyverse")
+# library(tidyverse)
 
 
 #rudimentary analysis of the dataset
@@ -46,5 +46,32 @@ plot(lifeExp ~ gdpPercap, gapminder)
 plot(lifeExp ~ log(gdpPercap), gapminder)
 hist(gapminder$lifeExp)
 
+table(gapminder$continent)
+barplot(table(gapminder$continent))
+barplot(table(gapminder$country))
 
 
+## we exploit the fact that ggplot2 was installed and loaded via the tidyverse
+p <- ggplot(filter(gapminder, continent != "Oceania"),
+            aes(x = gdpPercap, y = lifeExp)) # just initializes
+p <- p + scale_x_log10() # log the x axis the right way
+p + geom_point() # scatterplot
+p + geom_point(aes(color = continent)) # map continent to color
+p + geom_point(alpha = (1/3), size = 3) + geom_smooth(lwd = 3, se = FALSE)
+#> `geom_smooth()` using method = 'gam' and formula 'y ~ s(x, bs = "cs")'
+p + geom_point(alpha = (1/3), size = 3) + facet_wrap(~ continent) +
+  geom_smooth(lwd = 1.5, se = FALSE)
+#> `geom_smooth()` using method = 'loess' and formula 'y ~ x'
+
+
+
+#using dplyr package
+
+library(tidyverse)
+library(gapminder)
+gapminder
+
+filter(gapminder, between(lifeExp, 29, 40))
+filter(gapminder, lifeExp < 29 & lifeExp > 40)
+filter(gapminder, country == 'Rwanda', year > 1979)
+filter(gapminder, country %in% c('Rwanda', 'Afghanistan'))
