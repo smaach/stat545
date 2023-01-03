28dab123ac212a5316abb2a43d6d59b7c0dc5119
smaach <meetsavsani2000@gmail.com>
2023-01-03 17:03:37 +0530

    first commit

---
+++ Data Analysis I.R
@@ -0,0 +1,32 @@
+#This file contains all the practice I did
+#while learning from the course stat545
+#url: https://stat545.com/
+
+#ch5: Data Analysis
+
+#install necessary packages
+
+install.packages("gapminder")
+library(gapminder)
+install.packages("tidyverse")
+library(tidyverse)
+
+
+#rudimentary analysis of the dataset
+
+head(gapminder) # --> extract first few rows   
+summary(gapminder) # --> statistical overview the dataset
+str(gapminder) # --> get the structure of dataset
+class(gapminder) # --> understand the datatype
+as_tibble(gapminder) # --> explicitly convert dataset into tibble 
+names(gapminder) # --> columns of the dataset
+ncol(gapminder) # --> get no.of.columns
+length(gapminder) # --> same as ncol()
+dim(gapminder) # --> get the dimensions of dataset
+nrow(gapminder) # --> get the no.of.rows
+
+#very basic plots in R
+
+ggplot(gapminder, aes(year, lifeExp)) + geom_point()
+ggplot(gapminder, aes(gdpPercap, lifeExp)) + geom_point()
+plot(lifeExp ~ gdpPercap, gapminder)
